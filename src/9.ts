c810b7157acd187c4acfb0ad199b560675acbd95
Wiktoria Van Harneveldt <w.dev+wslgit@mailbox.org>
2023-12-14 17:29:57 +0100

    feat: solve day 9 part 2

--- src/9.ts
+++ src/9.ts
@@ -3,6 +3,7 @@ import { Solution } from './types';
 const solution = (input: string): Solution => {
   const lines = input.split('\n').map((line) => line.split(' ').map(Number));
   const result: number[] = [];
+  const negativResult: number[] = [];
 
   lines.forEach((line) => {
     let history = [line];
@@ -29,15 +30,18 @@ const solution = (input: string): Solution => {
 
     index--;
     let current = 0;
+    let prev = 0;
 
     for (index; index >= 0; index--) {
       current = history[index].at(-1)! + current;
+      prev = history[index].at(0)! - prev;
     }
     result.push(current);
+    negativResult.push(prev);
   });
 
   const part1 = result.reduce((acc, cur) => acc + cur, 0).toString();
-  const part2 = '';
+  const part2 = negativResult.reduce((acc, cur) => acc + cur, 0).toString();
   return { part1, part2 };
 };
 
